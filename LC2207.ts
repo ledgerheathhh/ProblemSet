fda4fdc0d7bee4349a47e235ed07f82092c1ce96
ledger-win11 <laored@outlook.com>
2024-09-24 23:52:48 +0800

    字符串中最多数目的子序列

--- LC2207.ts
+++ LC2207.ts
@@ -32,3 +32,17 @@
 // 1 <= text.length <= 105
 // pattern.length == 2
 // text 和 pattern 都只包含小写英文字母。
+
+function maximumSubsequenceCount(text: string, pattern: string): number {
+  let res = 0, cnt1 = 0, cnt2 = 0;
+  for (let c of text) {
+      if (c == pattern[1]) {
+          res += cnt1;
+          cnt2++;
+      }
+      if (c == pattern[0]) {
+          cnt1++;
+      }
+  }
+  return res + Math.max(cnt1, cnt2);
+};
